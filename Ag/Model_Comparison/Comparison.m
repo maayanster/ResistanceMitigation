ce69c3b795e5a498daa3788a9f8acaf73f29ab47
alsemm <alsemmelink@gmail.com>
2015-07-21 15:35:12 -0700

    This a new comparison file that includes comparison between stochastic and deterministic model and two batches where natural enemies are present in only the refuge and natural enemies are present in both refuge and treatment

--- Ag/Model_Comparison/Comparison.m
+++ Ag/Model_Comparison/Comparison.m
@@ -1,23 +1,18 @@
 %% DESCRIPTION
-% Comparison: runs both stochastic and deterministic models of
-%                        insecticide resistance with differing rates of one 
-%                        of the inputs (independent variable). Also
-%                        produces a graph that compares the number of
-%                        generations to resistance threshold and
-%                        independent variable between the two models
-
-%% INPUTS
-Num_sim = 5;                                       % Number of simulations that comparison is run
-q_freq_arr = [0.005, 0.005, 0.005, 0.005, 0.005];  % Initial frequency of resistant alleles
-K_arr = [42000, 42000, 42000, 42000, 42000];       % Carrying capacity
-Pref_arr = [0.3, 0.3, 0.3, 0.3, 0.3];              % Proportion of area that is refuge
-WErr_ref_arr = [1, 0.5, 0.4, 0.3, 0.2];            % Fitness of RR in refuge with natural enemies
-WErs_ref_arr = [1, 0.5, 0.5, 0.5, 0.5];            % Fitness of RS in refuge with natural enemies
-WEss_ref_arr= [1, 0.5, 0.5, 0.5, 0.5];             % Fitness of SS in refuge with natural enemies
-WErr_toxic_arr = [1, 1, 1, 1, 1];                  % Fitness of RR in toxic with natural enemies
-WErs_toxic_arr = [1, 1, 1, 1, 1];                  % Fitness of RS in toxic with natural enemies
-WEss_toxic_arr= [1, 1, 1, 1, 1];                   % Fitness of SS in toxic with natural enemies
-gen_num_arr = [5000, 5000, 5000, 5000, 5000];      % Number of generations models are run];    
+% Compares deterministic and stochastic model to find difference which is
+% assumed to be the abundance effect for a range of values.
+%% INPUTS - NE present only in refuge
+Num_sim = 10;                                                                         % Number of simulations that comparison is run
+q_freq_arr = [0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005];  % Initial frequency of resistant alleles
+K_arr = [42000, 42000, 42000, 42000, 42000, 42000, 42000, 42000, 42000, 42000];       % Carrying capacity
+Pref_arr = [0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3];                        % Proportion of area that is refuge
+WErr_ref_arr = [0, 0.05, 0.1, 0.15, 0.20,0.25,0.30,0.35,0.40,0.45];                % Fitness of RR in refuge with natural enemies
+WErs_ref_arr = [0.5, 0.5, 0.5, 0.5, 0.5,0.5,0.5,0.5,0.5,0.5];                    % Fitness of RS in refuge with natural enemies
+WEss_ref_arr = [0.5, 0.5, 0.5, 0.5, 0.5,0.5,0.5,0.5,0.5,0.5];                      % Fitness of SS in refuge with natural enemies
+WErr_toxic_arr = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];                                      % Fitness of RR in toxic with natural enemies
+WErs_toxic_arr = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];                                      % Fitness of RS in toxic with natural enemies
+WEss_toxic_arr = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];                                        % Fitness of SS in toxic with natural enemies
+gen_num_arr = [2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500];           % Number of generations models are run];      
 
 %% INITIALIZE
 median_stochastic = zeros(1,length(q_freq_arr));
@@ -28,51 +23,89 @@ median_det50 = zeros(1,length(q_freq_arr));
 gen2thresh_det_mean = zeros(1, length(q_freq_arr));
 
 %% CALCULATIONS
-% Call stochastic model
+% Using the parameters above call function that runs both models yielding 
+% generations to threshold for the stochastic model (median) and 
+% deterministic 
 for mm = 1:Num_sim
-    [gen2thresh_25percentile, gen2thresh_50percentile, ...
-        gen2thresh_75percentile, gen2thresh_mean, runs_below_thresh]= ...
-        Stochastic_Runs(q_freq_arr(mm), Pref_arr(mm), K_arr(mm), ...
+    display(mm)
+    [gen2thresh_sto_median, gen2thresh_det, gen2thresh_s_d]= ...
+        Runs(q_freq_arr(mm), Pref_arr(mm), K_arr(mm), ...
         WErr_ref_arr(mm), WErs_ref_arr(mm), WEss_ref_arr(mm), ...
         WErr_toxic_arr(mm), WErs_toxic_arr(mm), WEss_toxic_arr(mm),...
         gen_num_arr(mm));
-    median_stochastic(mm) = gen2thresh_50percentile;
-    gen2thresh_mean_stochastic(mm) = gen2thresh_mean;
-    runs_below_thresh_array(mm) = runs_below_thresh;
+    gen2thresh_sto_median_arr(mm) = gen2thresh_sto_median;
+    gen2thresh_det_arr(mm) = gen2thresh_det;
+    diff_s_d_arr(mm) = gen2thresh_s_d;
 end
 
-% Call deterministic model to match number of runs that reach threshold of
-% resistance for the 50th percentile stochastic model
-for mm = 1:length(median_stochastic)
-    [q_freq, gen2thresh] = Deterministic_13March(q_freq_arr(mm),...
-        Pref_arr(mm), WErr_ref_arr(mm), WErs_ref_arr(mm), ...
-        WEss_ref_arr(mm), WErr_toxic_arr(mm), WErs_toxic_arr(mm),...
-        WEss_toxic_arr(mm), gen_num_arr(mm));
-    median_det50(mm) = gen2thresh;
-end
+%% NORMALIZE
+%norm_stochastic = ((gen2thresh_sto_median_arr./gen2thresh_sto_median_arr(1,1))-1)*100;
+%norm_difference = ((diff_s_d_arr./diff_s_d_arr(1,1))-1)*100;
+%norm_deterministic = ((gen2thresh_det_arr./gen2thresh_det_arr(1,1))-1)*100;
 
-% Call deterministic model to match number of runs that reach threshold of
-% resistance for the mean stochastic model
-for mm = 1:length(gen2thresh_mean_stochastic)
-    [q_freq, gen2thresh] = Deterministic_13March(q_freq_arr(mm),...
-        Pref_arr(mm), WErr_ref_arr(mm), WErs_ref_arr(mm), ...
-        WEss_ref_arr(mm), WErr_toxic_arr(mm), WErs_toxic_arr(mm),...
-        WEss_toxic_arr(mm), gen_num_arr(mm));
-    gen2thresh_det_mean(mm) = gen2thresh;
-end
+
 %% COMPARE RESULTS
-% Find ratio of stochastic over deterministic for the number of generations
-% until resistant allele reaches threshold (frequency of 0.1)
-Gen_thresh_50s_d = median_stochastic./median_det50;
-Gen_thresh_means_d = gen2thresh_mean_stochastic./gen2thresh_det_mean;
-
 % Assign parameter that is varied on x-axis
-x = [0, 0.5, 0.6, 0.7, 0.8];
+x = [0, 0.025, 0.05, 0.075,0.1,0.125, 0.15, 0.175, 0.2, 0.225, 0.25,0.275,0.3,0.325,0.35,0.375,0.4,0.425,0.45,0.475,0.5];
 
 % Plot calculated stochastic over deterministic ratio of generations to 
 % threshold (y_axis) and the parameter that is varied - x (x_axis)
 figure
-plot(x, Gen_thresh_50s_d)
-xlabel('Mortality', 'FontSize', 12);
-ylabel('Generations to threshold (stochastic over deterministic ratio)',...
-    'FontSize', 12);
+subplot(2,1,1)
+plot(x, gen2thresh_sto_median_arr, x, gen2thresh_det_arr)
+ylabel('Generations to threshold','FontSize', 12);
+legend('Stochastic', 'Deterministic');
+
+%% INPUTS - NE present in both
+Num_sim_b = 10;                                                                         % Number of simulations that comparison is run
+q_freq_arr_b = [0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005];  % Initial frequency of resistant alleles
+K_arr_b = [42000, 42000, 42000, 42000, 42000, 42000, 42000, 42000, 42000, 42000];       % Carrying capacity
+Pref_arr_b = [0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3];                        % Proportion of area that is refuge
+WErr_ref_arr_b = [0, 0.05, 0.1, 0.15, 0.20,0.25,0.30,0.35,0.40,0.45];                % Fitness of RR in refuge with natural enemies
+WErs_ref_arr_b = [0.5, 0.5, 0.5, 0.5, 0.5,0.5,0.5,0.5,0.5,0.5];                    % Fitness of RS in refuge with natural enemies
+WEss_ref_arr_b = [0.5, 0.5, 0.5, 0.5, 0.5,0.5,0.5,0.5,0.5,0.5];                      % Fitness of SS in refuge with natural enemies
+WErr_toxic_arr_b = [0, 0.05, 0.1, 0.15, 0.20,0.25,0.30,0.35,0.40,0.45];                                      % Fitness of RR in toxic with natural enemies
+WErs_toxic_arr_b = [0.5, 0.5, 0.5, 0.5, 0.5,0.5,0.5,0.5,0.5,0.5];                                      % Fitness of RS in toxic with natural enemies
+WEss_toxic_arr_b = [0.5, 0.5, 0.5, 0.5, 0.5,0.5,0.5,0.5,0.5,0.5];                                        % Fitness of SS in toxic with natural enemies
+gen_num_arr_b = [2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500];           % Number of generations models are run];      
+
+%% INITIALIZE
+median_stochastic_b = zeros(1,length(q_freq_arr_b));
+gen2thresh_mean_stochastic_b = zeros(1,length(q_freq_arr_b));
+runs_below_thresh_array_b = zeros(1, length(q_freq_arr_b));
+
+median_det50_b = zeros(1,length(q_freq_arr_b));
+gen2thresh_det_mean_b = zeros(1, length(q_freq_arr_b));
+
+%% CALCULATIONS
+% Using the parameters above call function that runs both models yielding 
+% generations to threshold for the stochastic model (median) and 
+% deterministic 
+for mm = 1:Num_sim_b
+    display(mm)
+    [gen2thresh_sto_median_b, gen2thresh_det_b, gen2thresh_s_d_b]= ...
+        Runs(q_freq_arr_b(mm), Pref_arr_b(mm), K_arr_b(mm), ...
+        WErr_ref_arr_b(mm), WErs_ref_arr_b(mm), WEss_ref_arr_b(mm), ...
+        WErr_toxic_arr_b(mm), WErs_toxic_arr_b(mm), WEss_toxic_arr_b(mm),...
+        gen_num_arr_b(mm));
+    gen2thresh_sto_median_arr_b(mm) = gen2thresh_sto_median_b;
+    gen2thresh_det_arr_b(mm) = gen2thresh_det_b;
+    diff_s_d_arr_b(mm) = gen2thresh_s_d_b;
+end
+
+%% NORMALIZE
+%norm_stochastic_b = ((gen2thresh_sto_median_arr_b./gen2thresh_sto_median_arr_b(1,1))-1)*100;
+%norm_difference_b = ((diff_s_d_arr_b./diff_s_d_arr_b(1,1))-1)*100;
+%norm_deterministic_b = ((gen2thresh_det_arr_b./gen2thresh_det_arr_b(1,1))-1)*100;
+
+%% COMPARE RESULTS
+% Assign parameter that is varied on x-axis
+x_b = [50, 55, 60, 65, 70, 75, 80, 85, 90, 95];
+
+% Plot calculated stochastic over deterministic ratio of generations to 
+% threshold (y_axis) and the parameter that is varied - x (x_axis)
+subplot(2,1,2)
+plot(x_b, gen2thresh_sto_median_arr_b, x_b, gen2thresh_det_arr_b)
+xlabel('Mortality (%)', 'FontSize', 12);
+legend('stochastic', 'deterministic');
+
