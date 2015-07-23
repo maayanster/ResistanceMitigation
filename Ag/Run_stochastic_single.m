3040aa3e60e15edbbb2dab206de5b8fa83f9600b
alsemm <alsemmelink@gmail.com>
2015-07-23 10:56:22 -0700

    Updated description, changed the outputs for runs of stochastic model to generations to threshold, added a histogram function to plot generations to threshold and added an input so that we can more easily vary the number of times we run the stochastic model

--- Ag/Run_stochastic_single.m
+++ Ag/Run_stochastic_single.m
@@ -1,22 +1,40 @@
-%Run_stochastic_single
-% q_freq ----> frequency of resistant alleles
-% Pref ------> proportion of area that is refuge (no Bt)
-% WErr_ref --> fitness of RR in refuge with natural enemies
-% WErs_ref --> fitness of RS in refuge with natural enemies
-% WEss_ref --> fitness of SS in refuge with natural enemies
-% gen_num ---> number of generations (minus 1)
+%% DESCRIPTION of Run_stochastic_single 
+% Runs a stochastic model of insecticide resistance with a single set of 
+% parameters multiple times. The script also plots a histogram of the
+% number of generations to resistance for however many times the stochastic
+% model is run
+%% Structure of stochastic function:
+%[generations2thresh, q_array] = Stochastic(q_freq, Pref, K, ...
+% WErr_ref, WErs_ref, WEss_ref, WErr_toxic, WErs_toxic, WEss_toxic,...
+% gen_num)
+% Where
+% q_freq -----> Frequency of resistant alleles
+% Pref -------> Proportion of area that is refuge (no Bt)
+% K ----------> Carrying capacity
+% WErr_ref ---> Fitness of RR in refuge with natural enemies
+% WErs_ref ---> Fitness of RS in refuge with natural enemies
+% WEss_ref ---> Fitness of SS in refuge with natural enemies
+% WErr_toxic -> Fitness of RR in field due to natural enemies
+% WErs_toxic -> Fitness of RS in field due to natural enemies
+% WEss_toxic -> Fitness of SS in field due to natural enemies
+% gen_num ----> Number of generations 
 
-q_s = zeros(1,100);
-pops = zeros(1,100);
+%% INPUTS
+num_runs = 10;
 
-for nn=1:10                                              %number of trials
-    [q_freq, pop] = stochastic_model_05March(0.001,0.3, 0.5, 0.5, 0.5, 199);
-    q_s(nn) = q_freq;
-    pops(nn) = pop;
+%% INITIALIZE
+q_freq_arr = zeros(1,num_runs);
+gen2thresh_arr = zeros(1,num_runs);
+
+%% Call stochastic function multiple times
+for nn=1:num_runs                                        % number of trials
+    [generation2thresh, q_freq] = Stochastic(0.005,0.3,42000,...
+        0.5, 0.5, 0.5, 0.5, ...
+        0.5, 0.5, 2500);
+    gen2thresh_arr(nn) = generation2thresh;
 end
 
-resistant_not_extinct = sum(q_s>0); % how many trials did the R allele not go to 0
-display(resistant_not_extinct)
+%% Plot histogram of generations to threshold of multiple runs
+hist(gen2thresh_arr)
 
-resistant_fixation = sum(q_s>0.1); % how many trial did the R allele get to 0.1 of pop.
-display(resistant_fixation)
+
