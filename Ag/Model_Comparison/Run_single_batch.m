61c01599cffe65a5ac2508452302e86d40029544
alsemm <alsemmelink@gmail.com>
2015-08-18 14:55:37 -0700

    Added standard deviation calculation

--- Ag/Model_Comparison/Run_single_batch.m
+++ Ag/Model_Comparison/Run_single_batch.m
@@ -1,4 +1,4 @@
-function[gen2thresh_sto_median, gen2thresh_det,gen2thresh_s_d] = ...
+function[gen2thresh_sto_median, gen2thresh_det,gen2thresh_s_d,std_stochastic] = ...
     Run_single_batch(q_freq, Pref, K, WErr_ref, WErs_ref, WEss_ref,WErr_toxic, ...
     WErs_toxic,WEss_toxic, gen_num)
 %% Description: runs the stochastic model 100 times and pairs this with a
@@ -10,6 +10,7 @@ gen2thresh_det = 0;
 
 %% CALCULATIONS
 % Runs 100 simulations of stochastic model
+tic
 for nn = 1:100
         [generations2thresh] = Stochastic(q_freq,Pref, K, WErr_ref,...
             WErs_ref, WEss_ref, WErr_toxic, WErs_toxic, WEss_toxic, ...
@@ -19,7 +20,7 @@ for nn = 1:100
         gen2thresh_sto(nn) = generations2thresh;
         
 end 
-
+toc
 % Plot histogram of 100 runs of stochastic model to evaluate its
 % distribution
 %hist(gen2thresh_sto)
@@ -27,6 +28,9 @@ end
 % Find the median number of generations to threshold of 100 runs of
 % stochastic model
 gen2thresh_sto_median = prctile(gen2thresh_sto, 50);
+
+% Calculate standard deviation of 100 stochastic runs
+std_stochastic = std(gen2thresh_sto);
 
 % Call deterministic model for given parameter
 gen2thresh_det = Deterministic(q_freq,...
